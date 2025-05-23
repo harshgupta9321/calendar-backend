fb2b264849ddab910988014f9281403a21e0d297
harshgupta9321 <112786943+harshgupta9321@users.noreply.github.com>
2025-05-23 14:41:06 +0530

    delete school

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
-import { uploadAllSchoolExcel, createSchool,generateLink,getAllSchools } from '../controllers/schoolController.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { uploadAllSchoolExcel, createSchool,generateLink,getAllSchools,deleteSchool } from '../controllers/schoolController.js';
+// import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { checkRole } from '../middlewares/roleMiddleware.js';
 
 const router = express.Router();
@@ -11,7 +11,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/upload-all', upload.single('file'), uploadAllSchoolExcel); // Upload Excel for all schools
 router.post('/create', createSchool);  // Create a new school
-
+router.delete('/:schoolId',checkRole("admin"), deleteSchool);  // DELETE /schools/:schoolId
 router.get('/generate-link/:schoolId', generateLink);
 router.get('/all', getAllSchools);
 
